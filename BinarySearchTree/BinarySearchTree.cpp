f564cca90bd8171cdc58c2a2d03e1cdc27619d44
Michael Lee <michaellee.43@gmail.com>
2015-11-08 15:53:01 -0700

    Finished all functions. readmin and popmin dont return the correct values.

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -3,6 +3,7 @@
 template<class T>
 struct TreeNode {
     T data;             //Should this just be a reference to the data object?
+    int height;
     TreeNode* left;
     TreeNode* right;
     TreeNode* parent;
@@ -16,18 +17,17 @@ class AVLTree {
         void removeLeft(TreeNode<T>*);
         void removeRight(TreeNode<T>*);
         void removeBoth(TreeNode<T>*);
+        void adder(TreeNode<T>*, TreeNode<T>*);
+        int getHeight(TreeNode<T>*);
+        void rotateRight(TreeNode<T>*);
+        void rotateLeft(TreeNode<T>*);
     public:
         TreeNode<T>* getNode(T obj, TreeNode<T>*);
         void insert(T obj);
-        void addHelper(TreeNode<T>*, TreeNode<T>*);
         T* readMinNode();
         T* popMinNode();
         void remove(T obj);   //TODO: Different name from the specification
-    
-    
-        /*void balance(TreeNode*);
-        void rotateRight(TreeNode*);
-        void rotateLeft(TreeNode*);*/
+        void balance(TreeNode<T>*);
     
         AVLTree() {
             root = NULL;
@@ -35,7 +35,7 @@ class AVLTree {
 };
 
 
-template <class T>                                             //This may require an additions parameter
+template <class T>              //This may require an additions parameter
 TreeNode<T>* AVLTree<T>::getNode(T obj, TreeNode<T>* currentNode){
     if(currentNode == NULL) {
         return NULL;
@@ -54,7 +54,7 @@ template <class T>
     while (currentNode->left != NULL) {
         currentNode = currentNode->left;
     }
-    return &(currentNode->data);       //This right?
+    return &(currentNode->data);       //This right? i get address of node
 }
 
 template <class T>
@@ -63,8 +63,8 @@ T* AVLTree<T>::popMinNode() {
     while (currentNode->left != NULL) {
         currentNode = currentNode->left;
     }
-    T* data = &(currentNode->data);               //This right?
-    remove(currentNode);
+    T* data = &(currentNode->data);               //This right? i get address of node
+    remove(currentNode->data);          //This right?
     return data;
 }
 
@@ -77,28 +77,28 @@ void AVLTree<T>::insert(T obj) {
     if (root == NULL){
         root = nodeToInsert;
     } else {
-        addHelper(nodeToInsert, root);
+        adder(nodeToInsert, root);
     }
         
-    //balance(nodeToInsert);
+    balance(nodeToInsert);
 }
 
 
 template <class T>                      //Is this necessary?
-void AVLTree<T>::addHelper(TreeNode<T>* nodeToInsert, TreeNode<T>* currentNode) {
+void AVLTree<T>::adder(TreeNode<T>* nodeToInsert, TreeNode<T>* currentNode) {
     if (nodeToInsert->data <= currentNode->data) {              //Comparisons may require different method
         if (currentNode->left == NULL) {
             currentNode->left = nodeToInsert;
             nodeToInsert->parent = currentNode;
         } else {
-            addHelper(nodeToInsert, currentNode->left);
+            adder(nodeToInsert, currentNode->left);
         }
     } else {
         if (currentNode->right == NULL) {
             currentNode->right = nodeToInsert;
             nodeToInsert->parent = currentNode;
         } else {
-            addHelper(nodeToInsert, currentNode->right);
+            adder(nodeToInsert, currentNode->right);
         }
     }
 }
@@ -121,7 +121,7 @@ void AVLTree<T>::remove(T obj) {
         removeBoth(nodeToRemove);
     }
     
-    //balance(nodeToRemove->parent);
+    balance(nodeToRemove->parent);
 }
 
 template <class T>
@@ -198,9 +198,149 @@ void AVLTree<T>::removeBoth(TreeNode<T>* nodeToRemove) {
 }
 
 
+template <class T>
+void AVLTree<T>::balance(TreeNode<T>* nodeToBalance) {
+    //End function when it has gone above the parent
+    if (nodeToBalance == NULL) {
+        return;
+    }
+    
+    //Get the height of right and left nodes
+    int rightHeight = getHeight(nodeToBalance->right);
+    int leftHeight = getHeight(nodeToBalance->left);
+    
+    //Rotate the tree based on 4 cases
+    if (rightHeight > leftHeight + 1) {
+        int rightLeftHeight = getHeight(nodeToBalance->right->left);
+        int rightRightHeight = getHeight(nodeToBalance->right->right);
+        if (rightRightHeight >= rightLeftHeight) {
+            rotateLeft(nodeToBalance);
+        } else {
+            rotateRight(nodeToBalance->right);
+            rotateLeft(nodeToBalance);
+        }
+    } else  if (rightHeight + 1 < leftHeight) {
+        int leftRightHeight= getHeight(nodeToBalance->left->right);
+        int leftLeftHeight= getHeight(nodeToBalance->left->left);
+        if (leftLeftHeight >= leftRightHeight) {
+            rotateRight(nodeToBalance);
+        } else {
+            rotateLeft(nodeToBalance->left);
+            rotateRight(nodeToBalance);
+        }
+    //Update height of node to balance
+    } else {
+        if (leftHeight >= rightHeight) {
+            nodeToBalance->height = leftHeight + 1;
+        } else {
+            nodeToBalance->height = rightHeight + 1;
+        }
+    }
+    
+    balance(nodeToBalance->parent);
+    
+}
+
+
+template <class T>
+int AVLTree<T>::getHeight(TreeNode<T>* node) {
+    if (node == NULL) {
+        return 0;
+    } else {
+        return node->height;
+    }
+}
+
+template <class T>
+void AVLTree<T>::rotateRight(TreeNode<T>* nodeToRotate) {
+    //Place new root into a temporary node
+    TreeNode<T>* updatedRoot = nodeToRotate->left;
+    nodeToRotate->left->parent = nodeToRotate->parent;
+    
+    if (nodeToRotate->parent == NULL) {
+        root = updatedRoot;
+    } else {
+        if (nodeToRotate == nodeToRotate->parent->right) {
+            nodeToRotate->parent->right = updatedRoot;
+        } else {
+            nodeToRotate->parent->left = updatedRoot;
+        }
+    }
+    
+    //Reassign child of new root
+    nodeToRotate->left = updatedRoot->right;
+    if (nodeToRotate->left != NULL) {
+        nodeToRotate->left->parent = nodeToRotate;
+    }
+    
+    updatedRoot->right = nodeToRotate;
+    nodeToRotate->parent = updatedRoot;
+    
+    //Update the height of the original root
+    int leftHeight = getHeight(nodeToRotate->left);
+    int rightHeight = getHeight(nodeToRotate->right);
+    if (leftHeight >= rightHeight) {
+        nodeToRotate->height = leftHeight + 1;
+    } else {
+        nodeToRotate->height = rightHeight + 1;
+    }
+    
+}
+
+template <class T>
+void AVLTree<T>::rotateLeft(TreeNode<T>* nodeToRotate) {
+    //Place new root into a temporary node
+    TreeNode<T>* updatedRoot = nodeToRotate->right;
+    nodeToRotate->right->parent = nodeToRotate->parent;
+    
+    if (nodeToRotate->parent == NULL) {
+        root = updatedRoot;
+    } else {
+        if (nodeToRotate == nodeToRotate->parent ->right) {
+            nodeToRotate->parent->right = updatedRoot;
+        } else {
+            nodeToRotate->parent->left = updatedRoot;
+        }
+    }
+    
+    //Reassign child of new root
+    nodeToRotate->right = updatedRoot->left;
+    if (nodeToRotate->right != NULL) {
+        nodeToRotate->right->parent = nodeToRotate;
+    }
+    
+    updatedRoot->left = nodeToRotate;
+    nodeToRotate->parent = updatedRoot;
+    
+    //Update the height of the original root
+    int leftHeight = getHeight(nodeToRotate->left);
+    int rightHeight = getHeight(nodeToRotate->right);
+    if (leftHeight >= rightHeight) {
+        nodeToRotate->height = leftHeight + 1;
+    } else {
+        nodeToRotate->height = rightHeight + 1;
+    }
+    
+}
+
 int main(int argc, const char * argv[]) {
     AVLTree<int> tree;
-    tree.insert(17);
+    
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+    tree.insert(4);
+    tree.insert(5);
+    tree.insert(6);
+    
+    int* min = tree.readMinNode();
+    int* min2 = tree.popMinNode();
+    
+    tree.remove(1);
+    tree.remove(2);
+    tree.remove(5);
+    
+    /*tree.insert(17);
     tree.insert(21);
     tree.insert(6);
     tree.insert(1);
@@ -208,9 +348,8 @@ int main(int argc, const char * argv[]) {
     tree.insert(2);
     tree.insert(19);
     tree.insert(34);
-    
     tree.remove(19);
     tree.remove(1);
-    tree.remove(17);
+    tree.remove(17);*/
     std::cout<<"Finish";
 }
